233d9b732c16f68d5bd2d4b5ff301e77241920fd
cmhaines215 <110734247+cmhaines215@users.noreply.github.com>
2022-10-02 10:06:47 -0400

    nav bar styling/ids done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,26 @@
-// this is the starting point for this commentary,
-// please add more steps
-// there may be more steps and corrections to be made in future
 
+
+var questions = [{
+  q1:"question1?",
+  a1:"answer1",
+answers:[1,2,3,4]
+},
+{
+
+},];
 // need a collection of objects that hold the questions
 // need to keep a count of the question that is displayed
 // need to keep score
 // need to keep the time
-
+questions[0].q1;
+questions[0].answers[0];
 
 function startTimer(){
 
 }
 function addQuestion(){
+
+ 
   // create the question elements
 
   // modify the text/attributes
@@ -21,27 +30,11 @@ function addQuestion(){
 
   // for all the buttons, add event listener
 
-    // WHEN I answer a question
-    // if answered correctly
-    // add points
-    // show as correct
-    // WHEN I answer a question incorrectly
-    // THEN time is subtracted from the clock
-    // show as incorrect
-
-    // after the answer, 
-    // THEN I am presented with another question
-    // need a function call to present the question
-
-    // if no other questions,
-    // go to highscore screen
 }
+addQuestion();
 
 function startQuiz(){
-  // timer starts and I am presented with a question
-  // need a function call to start the timer
-
-  // need a function call to present the question
 }
 
 // eventListener here // when i click the start button
+// for start
