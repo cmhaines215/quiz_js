7ab231614910d9434877bb4a0614ea4fde17b83f
cmhaines215 <110734247+cmhaines215@users.noreply.github.com>
2022-10-09 08:50:23 -0400

    trying again from scratch, have error reading questions array, but a lot of code is written

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,119 +1,109 @@
-var ans = [];
-var contAns =document.querySelector("#container-answers");
-var contQues =document.querySelector("#container-question");
-var mainCont =document.querySelector("#quiz-container");
+// created questions as array of objects
+var questions =[
+  {
+    q: "Question 1?",
+    id: 1,
+    answers: ["a1", "a2", "a3", "a4"],
+    correct: "a3"
+  },
+  {
+    q: "Question 2?",
+    id: 2,
+    answers: ["a1", "a2", "a3", "a4"],
+    correct: "a1"    
+  },
+  {
+    q: "Question 3?",
+    id: 3,
+    answers: ["a1", "a2", "a3", "a4"],
+    correct: "a4"
+  },
+  {
+    q: "Question 4?",
+    id: 4,
+    answers: ["a1", "a2", "a3", "a4"],
+    correct: "a2"
+  },
+  {
+    q: "Question 5?",
+    id: 5,
+    answers: ["a1", "a2", "a3", "a4"],
+    correct: "a4"
+  }
+];
 
-
-// need a collection of objects that hold the questions
-var questionsList = [{
-  question:"question1?",
-  answers:["answer1","answer2","answer3","answer4"],
-  correctAnswer:["answer2"],
-},
-{
-  question:"question2?",
-  answers:["answer1","answer2","answer3","answer4"],
-  correctAnswer:["answer4"],
-},
-{
-  question:"question3?",
-  answers:["answer1","answer2","answer3","answer4"],
-  correctAnswer:["answer3"],
-},
-{
-  question:"question4?",
-  answers:["answer1","answer2","answer3","answer4"],
-  correctAnswer:["answer1"],
-},
-{
-  question:"question5?",
-  answers:["answer1","answer2","answer3","answer4"],
-  correctAnswer:["answer2"],
-}];
-
-
+var currentQ = 0;
+var ans = 0;
+var containerAnswers = document.querySelector("#container-answers");
+var containerQuestion = document.querySelector("container-question");
 
 function startScreen(){
-  // starts at zero
-  ans = [];
-// create
   var item = document.createElement("li");
   var btn = document.createElement("button");
-//modify/add attributes
-  btn.textContent = "start";
+
+  btn.textContent = "Start";
   btn.setAttribute("id", "start-button");
-//append to page
-  contAns.appendChild(item);
+  // btn.style("margin-right", "30px");
+
+  containerAnswers.appendChild(item);
   item.appendChild(btn);
-//add heading
-//modify/add attributes
-contQues.textContent = "What Button can you click to start the quiz? (zero credit for this one)"
-
 
 }
-// call start screen first
+function quizQuestions(){
+  var question = questions[currentQ];
+  containerQuestion.textContent = question.q;
+  // clear area for answers
+  containerAnswers.innerHTML = '';
+
+  for(var i = 0; i < question.answers.length; i++){
+    console.log(question.answers[i]);
+
+    var item =document.createElement('li');
+    var btn = document.createElement('button');
+
+    btn.setAttribute("class","option-button");
+    btn.textContent =question.answers[i];
+    btn.dataset.index = currentQ;
+    btn.dataset.next = currentQ + 1;
+
+    if(question.answers === question.correct){
+      btn.dataset.value = "correct";
+    }
+    else{
+      btn.dataset.value = "incorrect";
+
+    }
+    containerAnswers.appendChild(item);
+    item.appendChild(btn);
+  }
+}
+// calling start screen function to the page first
 startScreen();
-// need to keep a count of the question that is displayed
-// need to keep score
-// need to keep the time
-function startTimer(){
 
-}
-function addQuestion(){
+containerAnswers.addEventListener("click", function(event){
+  // console.log(ans);
 
-  var currentQ = 0;
-  var quizQuestion = questionsList[0];
-
-  contQues.textContent = quizQuestion.question;
-
-//clear start button
-  contAns.innerHTML = '';
-
-
-
- for(var i = 0; i < quizQuestion.answers.length; i++){
-  console.log(quizQuestion.answers[i]);
-   // create the question elements
-   var item = document.createElement('li');
-   var button = document.createElement('button');
-  //modify
-  button.setAttribute("class","option-button");
-  button.textContent = button.textContent = quizQuestion.answers;
-  button.dataset.index = currentQ;
-  button.dataset.next = currentQ + 1;
-  if(quizQuestion.answers[i] === quizQuestion.incorrectanswers){
-    button.dataset.value = "incorrect";
-  }
-  else{
-    button.dataset.value = "correct";
-  }
-  var item = document.createElement("li");
-  var btn = document.createElement("button");
-    //append
-    contAns.appendChild(item);
-    item.appendChild(button);
- }
- 
-
-  // modify the text/attributes
-  // modify one of the attributes to say right or wrong
-
-  // append all elements to something on the page
-
-  // for all the buttons, add event listener
-
-}
-
-function startQuiz(){
-}
-
-// eventListener here // when i click the start button
-// for start
-contAns.addEventListener("click", function(event){
   var child = event.target;
   console.log(child);
 
   if(child.matches("#start-button")){
-    addQuestion();
+    quizQuestions();
+  }
+  if(child.matches(".option-button")){
+    if(child.dataset.next < questions.length){
+      currentQ = parseInt(child.dataset.next);
+      if(child.dataset.value !== "correct"){
+        ans++;
+      }
+      quizQuestions();
+    }
+    else{
+      if(child.dataset.value !== "correct"){
+        ans++;
+      }
+      containerQuestion.textContent = "You got " + ans + "questions right, way to go.";
+      // save to local storage
+      containerAnswers.innerHTML = "";
+    }
   }
 })
